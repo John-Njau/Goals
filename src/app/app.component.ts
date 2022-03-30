fe797997dc0b68e9c72456e71729e398a4062ac1
John <john.njoroge@student.moringaschool.com>
2022-03-30 23:30:36 +0300

    ft: Updating contents of a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  // defining an array
+  goals:string[];
+
+  // constructor function that adds 3 values to our empty array
+  constructor(){
+    this.goals = ['Watch Finding Nemo', 'Buy biscuit', 'Get new phone case']
+  }
+
+  // defining a variable
+  title = 'My Goals'
 }
