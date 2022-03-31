f1ee05741f8dac4599429dd2a0cfb667ee276efa
John <john.njoroge@student.moringaschool.com>
2022-03-31 14:29:43 +0300

    ft: Added a custom directive that takes in a user initiated event to cross out and undo the cross on the goals when clicked and double ckicked

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,12 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor(private elem:ElementRef) { 
-    this.elem.nativeElement.style.textDecoration='line-through'
+  // constructor(private elem:ElementRef){
+  //   this.elem.nativeElement.style.textDecoration='line-through'
+  // }
+
+  constructor(private elem: ElementRef) { }
+
+  // actions taken in by the textDeco function
+  @HostListener('click') onClicks() {
+    this.textDeco('line-through')
+  }
+
+  @HostListener('dblclick') onDoubleClicks() {
+    this.textDeco('None')
+  }
+
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.textDecoration = action;
   }
 
 }
