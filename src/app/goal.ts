4d3e57edbe3e2a0b21fd2dc0bc54a2c3fe560e97
John <john.njoroge@student.moringaschool.com>
2022-03-31 13:14:26 +0300

    ft: Added a click event to goal component to toggle the details of goals

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,10 +1,6 @@
 export class Goal {
-    id!: number;
-    name!: string;
-    description!: string;
-
-    // constructor (){
-    //     this.id
-    //     this.name
-    // }
+    showDescription: boolean;
+    constructor( public id:  number,public name: string, public description: string) {
+        this.showDescription = false;
+    }
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -10,13 +10,18 @@ import { Goal } from '../goal'
 export class GoalComponent implements OnInit {
 
   goals: Goal[] = [
-    { id: 1, name: 'Watch finding Nemo', description:'Find an online version' },
-    { id: 2, name: 'Buy Cookies', description: 'I have to buy cookies for my people' },
-    { id: 3, name: 'Get new Phone Case', description: 'Linda has her bday coming up' },
-    { id: 4, name: 'Get Dog Food', description: 'Linda has her bday coming up' },
-    { id: 5, name: 'Solve math homework', description: 'Linda has her bday coming up' },
-    { id: 6, name: 'Plot my world domination plan', description: 'Linda has her bday coming up' },
+    new Goal(1, 'Watch finding Nemo', 'Find an online version'),
+    new Goal(2, 'Buy Cookies', 'I have to buy cookies for my people'),
+    new Goal(3, 'Get new Phone Case', 'Linda has her bday coming up'),
+    new Goal(4, 'Get Dog Food', 'Linda has her bday coming up'),
+    new Goal(5, 'Solve math homework', 'Linda has her bday coming up'),
+    new Goal(6, 'Plot my world domination plan', 'Linda has her bday coming up'),
   ];
+
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
