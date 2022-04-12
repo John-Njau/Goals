5590b17e0476d08a53415ddd28fa531026bedc89
john.njau <john.njoroge@student.moringaschool.com>
2022-04-12 14:29:28 +0300

    Add goal List clas

---
+++ src/app/goal-list.ts.spec.ts
@@ -0,0 +1,7 @@
+import { GoalList.Ts } from './goal-list.ts';
+
+describe('GoalList.Ts', () => {
+  it('should create an instance', () => {
+    expect(new GoalList.Ts()).toBeTruthy();
+  });
+});
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,15 +9,6 @@ import { Goal } from '../goal'
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version', new Date(2021, 16, 9)),
-    new Goal(2, 'Buy Cookies', 'I have to buy cookies for my people', new Date(2020, 6, 12)),
-    new Goal(3, 'Get new Phone Case', 'Linda has her bday coming up', new Date(2019, 3, 13)),
-    new Goal(4, 'Get Dog Food', 'Linda has her bday coming up', new Date(2022, 5, 12)),
-    new Goal(5, 'Solve math homework', 'Linda has her bday coming up', new Date(2022, 4, 28)),
-    new Goal(6, 'Plot my world domination plan', 'Linda has her bday coming up', new Date(2022, 11, 31)),
-    new Goal(7, 'Go to Town', 'At 1 pm', new Date(2022, 1, 4))
-  ];
 
   addNewGoal(goal){
     let goalLength = this.goals.length;
